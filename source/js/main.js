4fc3f5e4907e097475f4904e4d7dc5ad2effc564
jason-bowen-zheng <18917572736@wo.cn>
2020-11-29 05:33:20 +0800

    2020-11-29

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,20 @@
-function data2issues(data, repo) {
+function data2detail(data, repo) {
 	url = "https://github.com/" + repo + "/issues/";
 	s = '<li>';
 	s+= '<a href="' + url + data["number"] + '">' + data["title"] + "</a>";
-	s+= " by " + data["user"]["login"] + "</li>";
+	s+= " #" + data["number"] + " by " + data["user"]["login"] + "</li>";
 	return s;
+}
+
+function setIssuesList(repo, id) {
+	api = "https://api.github.com/repos/" + repo + "/issues";
+	$.get(api, function(data, status) {
+		if (status == "success") {
+			for (issues of data) {
+				$(id).prepend(data2detail(issues, repo));
+			}
+		} else {
+			$(id).prepend("<li>Github api error!</li>");
+		}
+	});
 }
