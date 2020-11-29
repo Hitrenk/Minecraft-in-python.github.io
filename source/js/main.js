cacdf91411b6ec89ca311ac5e21233ce9329240d
jason-bowen-zheng <18917572736@wo.cn>
2020-11-29 10:42:48 +0800

    2020-11-29

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,29 @@
-function data2detail(data, repo) {
-	url = "https://github.com/" + repo + "/issues/";
+function data2articleDetail(data) {
+	url = "./article-tmp.html?" + data["name"];
 	s = '<li>';
-	s+= '<a href="' + url + data["number"] + '">' + data["title"] + "</a>";
-	s+= " #" + data["number"] + " by " + data["user"]["login"] + "</li>";
+	s += '<a href="' + url + '">' + data["name"].slice(0, -5) + "</a></li>";
 	return s;
+}
+
+function data2issuesDetail(data, repo) {
+	url = "https://github.com/" + repo + "/issues/" + data["number"];
+	s = '<li>';
+	s+= '<a href="' + url + '">' + data["title"] + "(#" + data["number"] + ")</a>";
+	s+= ' by <a href="' + data["user"]["html_url"] + '">' + data["user"]["login"] + "</a></li>";
+	return s;
+}
+
+function setArticleList(id) {
+	api = "https://api.github.com/repos/Minecraft-in-python/Minecraft/contents/article";
+	$.get(api, function(data, status) {
+		if (status == "success") {
+			for (file in data) {
+				$(id).prepend(data2articleDetail(data));
+			}
+		} else {
+			$(id).prepend("<li>Github api error!</li>");
+		}
+	});
 }
 
 function setIssuesList(repo, id) {
@@ -11,7 +31,7 @@ function setIssuesList(repo, id) {
 	$.get(api, function(data, status) {
 		if (status == "success") {
 			for (issues of data) {
-				$(id).prepend(data2detail(issues, repo));
+				$(id).prepend(data2issuesDetail(issues, repo));
 			}
 		} else {
 			$(id).prepend("<li>Github api error!</li>");
