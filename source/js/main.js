843a2e7404357e2084a0d6907b10babf4aae13e9
jason-bowen-zheng <18917572736@wo.cn>
2020-12-01 23:18:56 +0800

    2020-12-01

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,5 @@
+var count = -1;
+
 function data2articleDetail(data) {
 	url = "./article-tmp.html?" + data["name"];
 	s = '<li>';
@@ -9,12 +11,47 @@ function data2articleDetail(data) {
 	return s;
 }
 
+function data2docsDetail(type, id, name, extra="") {
+	if (type == "file") {
+		url = "./docs-tmp.html?" + name;
+		s = '<li id="'+ id + '">';
+		s += '<a href="' + url + "</a></li>";
+		return s;
+	} else {
+		s = "<li>" + name + "<ul>" + extra  + "</ul></li>";
+		return s;
+	}
+}
+
 function data2issuesDetail(data, repo) {
 	url = "https://github.com/" + repo + "/issues/" + data["number"];
 	s = '<li>';
 	s+= '<a href="' + url + '">' + data["title"] + "(#" + data["number"] + ")</a>";
 	s+= ' by <a href="' + data["user"]["html_url"] + '">' + data["user"]["login"] + "</a></li>";
 	return s;
+}
+
+function getDocs(path, id, callback=function() {}) {
+	api = "https://api.github.com/repos/Minecraft-in-python/Minecraft-in-python.github.io/contents/docs" + path;
+	$.get(api, function(data, status) {
+		if (status == "success") {
+			for (file of data) {
+				++ count;
+				if (file["type"] == "file") {
+					$(id).append(data2docsDetail("file", count, data["name"]));
+				} else {
+					if (path == "") {
+						getDocs(data["name"], count);
+					} else {
+						getDocs(path + "/" + data["name"], count);
+					}
+				}
+			}
+			callback();
+		} else {
+			$(id).append("<li>Github api error!</li>");
+		}
+	});
 }
 
 function getRecentCommit(repo, id) {
@@ -29,28 +66,30 @@ function getRecentCommit(repo, id) {
 	});
 }
 
-function setArticleList(id) {
+function setArticleList(id, callback=function() {}) {
 	api = "https://api.github.com/repos/Minecraft-in-python/Minecraft-in-python.github.io/contents/article";
 	$.get(api, function(data, status) {
 		if (status == "success") {
 			for (file of data) {
-				$(id).prepend(data2articleDetail(file));
+				$(id).append(data2articleDetail(file));
 			}
+			callback();
 		} else {
-			$(id).prepend("<li>Github api error!</li>");
+			$(id).append("<li>Github api error!</li>");
 		}
 	});
 }
 
-function setIssuesList(repo, id) {
+function setIssuesList(repo, id, callback=function() {}) {
 	api = "https://api.github.com/repos/" + repo + "/issues";
 	$.get(api, function(data, status) {
 		if (status == "success") {
 			for (issues of data) {
-				$(id).prepend(data2issuesDetail(issues, repo));
+				$(id).append(data2issuesDetail(issues, repo));
 			}
+			callback();
 		} else {
-			$(id).prepend("<li>Github api error!</li>");
+			$(id).append("<li>Github api error!</li>");
 		}
 	});
 }
